bcdacc624aa46a07b348911b1f7df29a9a4b175d
YASUO0715 <yasuo0715niihori@gmail.com>
2021-10-02 06:56:02 +0900

    servicecontainer

---
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LifeCycleTestController extends Controller
+{
+    public function showServiceContainerTest()
+    {
+        app()->bind('lifeCycleTest',function(){
+            return 'ライフサイクルテスト';
+        });
+        $test = app()->make('lifeCycleTest');
+
+        //サービスコンテナ無しのパターン。それぞれのクラスをいちいちインスタンス化してあげれば使える。
+        // $message = new Message();
+        // $sample = new Sample($message);
+        // $sample->run();
+
+        //サービスコンテナありのパターン. newをしなくてもインスタンス化できる。
+        //app()->make だけで依存関係を解決して簡単に使用できる。
+        app()->bind('sample',Sample::class);
+        $sample = app()->make('sample');
+        $sample -> run();
+
+        dd($test,app());
+    }
+}
+
+class sample
+{
+    public $message;            
+    //引数にクラスを入れてあげると、自動的にインスタンス化する。
+    public function __construct(Message $message){
+        $this->message = $message;
+}
+    public function run(){
+        $this->message->send();
+}
+}
+
+class Message 
+{
+    public  function send()
+    {
+        echo ('メッセージを表示');
+    }
+
+}
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,9 @@
 <x-tests.app>
+    {{-- スロットを複数使う時は名前付きで下記のようにname設定する。 --}}
+<x-slot name="header">ヘッダー2</x-slot>
 コンポーネントテスト2
+<x-test-class-base classBaseMessage="メッセージです"/>
+<div class="mb-4"></div>
+<x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更しています。"/>
+
 </x-tests.app>
