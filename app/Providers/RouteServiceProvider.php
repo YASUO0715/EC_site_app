9387ff1757fd7eff2e359bffabe40716d3ad4220
YASUO0715 <yasuo0715niihori@gmail.com>
2021-10-07 21:40:06 +0900

    マルチログイン

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -1,4 +1,4 @@
-R_HOME<?php
+<?php
 
 namespace App\Providers;
 
@@ -19,7 +19,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public const HOME = '/dashboard';
     public const OWNER_HOME = '/owner/dashboard';
-    public const ADMIN_HOME ='/admin/dashboard';
+    public const ADMIN_HOME = '/admin/dashboard';
 
     /**
      * The controller namespace for the application.
@@ -45,24 +45,24 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
-                Route::prefix('/')
+            Route::prefix('/')
                 ->as('user.')
                 ->middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
 
-                Route::prefix('owner')
+            Route::prefix('owner')
                 ->as('owner.')
                 ->middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/owner.php'));
 
-                Route::prefix('admin')
+            Route::prefix('admin')
                 ->as('admin.')
                 ->middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/admin.php'));
-            });
+        });
     }
 
     /**
